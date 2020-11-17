eae837d9986d5245e0f54bcba20cb1249de75222
Denatyeti <61144946+Denatyeti@users.noreply.github.com>
2020-11-17 14:14:55 +0100

    Update til while loop for at bevæge myre

--- Del 2/Simulator.java
+++ Del 2/Simulator.java
@@ -22,15 +22,23 @@ public class Simulator{
 	
 	public void tick(){
 		this.graph.tick();
+		RandomUtils random = new RandomUtils();
 		int i = 0,
-			j = 0;
+			j = 0,
+			k = 0;
+			
+		double probability;
 		// variables that calls a method once to avoid multiple calls of the same method.	
 		boolean isHome,
 				isStocked,
 				isCarrying,
-				wasHome;
+				wasHome,
+				hasMoved;
 		
-		Node current; 
+		Node current,
+			 previous,
+			 n; 
+		Node [] nodes;
 		
 		//alle myrer dør
 		//måske lave en counter som springer dette over på første tick.
@@ -57,6 +65,8 @@ public class Simulator{
 			}else{
 				isCarrying = ants[j].carrying(); // updating value to avoid multiple calls.
 				current = ants[j].current();
+				previous = ants[j].previous();
+				nodes = graph.adjacentTo(ants[j].current());
 				if(!(isCarrying) && current.sugar() > 0){
 					ants[j].pickUpSugar(); 
 					current.decreaseSugar();
@@ -64,8 +74,24 @@ public class Simulator{
 					if (ants[j].isAtHome() && ants[j].carrying()){ // checks the precondition. if true drop sugar into the colony.
 						ants[j].dropSugar();
 					}
+				}else if(nodes.length == 1){ //incase of only one adjacent node, move to that node.
+					ants[j].move(nodes[0]);
 				}else{
-					//move
+					hasMoved = false;
+					while(k < nodes.length && !(hasMoved)){
+						n = nodes[k];
+						probability = probability + formula(n,previous);
+						if(random.coinFlip(probability) && n != previous){
+							ants[j].move(n);
+							hasMoved = true;
+						}else if(k = (nodes.length-1) && n != previous){
+								ants[j].move(n);
+						}else if(k = (nodes.length-1) && n == previous){
+								ants[j].move(nodes[k-1]);
+						}else{
+							k = k + 1;						
+						}
+					}
 					
 				}
 			}
@@ -74,4 +100,7 @@ public class Simulator{
 		
 	}
 	
+	private double formula(Node n);
+	
+
 }
